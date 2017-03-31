7cad6a33e2e6c013a4603391ef54c854acf0f5f8
Alexis Suter <fisy43@gmail.com>
2017-03-31 14:52:34 +0200

    Semantic-UI style added Material-Crap removed

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,87 +1,92 @@
 // @flow
 
 import React from 'react'
-import { Redirect, Link } from 'react-router-dom'
+import {Redirect, Link} from 'react-router-dom'
+import {Card} from 'semantic-ui-react'
 
 export type Props = {
-  /* Callback to submit an authentication request to the server */
-  authenticate: (login: string, password: string, callback: (error: ?Error) => void) => void,
-  /* We need to know what page the user tried to access so we can 
+    /* Callback to submit an authentication request to the server */
+    authenticate: (login: string, password: string, callback: (error: ?Error) => void) => void,
+    /* We need to know what page the user tried to access so we can
      redirect after logging in */
-  location: {
-    state?: {
+    location: {
+        state?: {
 
-      from: string,
+            from: string,
+        }
     }
-  }
 }
 
 class Login extends React.Component {
-  
-  props: Props
-  
-  state: {
-    login: string,
-    password: string,
-    error?: Error,
-    redirectToReferrer: boolean,
-  }
-  
-  state = {
-    login: "",
-    password: "",
-    error: undefined,
-    redirectToReferrer: false,
-  }
 
-  handleLoginChanged = (event: Event) => {
-    if(event.target instanceof HTMLInputElement) {
-      this.setState({login: event.target.value})
+    props: Props
+
+    state: {
+        login: string,
+        password: string,
+        error?: Error,
+        redirectToReferrer: boolean,
     }
-  }
 
-  handlePasswordChanged = (event: Event) => {
-    if(event.target instanceof HTMLInputElement) {
-      this.setState({password: event.target.value})
+    state = {
+        login: "",
+        password: "",
+        error: undefined,
+        redirectToReferrer: false,
     }
-  }
 
-  handleSubmit = (event: Event) => {
-    event.preventDefault()
-    const { login, password } = this.state
-    this.props.authenticate(login, password, (error) => {
-      if(error) {
-        this.setState({error})
-      } else {
-        this.setState({redirectToReferrer: true, error: null})
-      }
-    })
-  }
-
-  render() {
-    const { from } = this.props.location.state || { from: { pathname: '/dashboard' } }
-    const { redirectToReferrer, error } = this.state
-    
-    if (redirectToReferrer) {
-      return (
-        <Redirect to={from}/>
-      )
+    handleLoginChanged = (event: Event) => {
+        if (event.target instanceof HTMLInputElement) {
+            this.setState({login: event.target.value})
+        }
     }
-        
-    return (
-      <div>
-        <h1>Bank of Rapperswil</h1>
-        <form>
-          <h2>Login</h2>
-          <input onChange={this.handleLoginChanged} placeholder='Login' value={this.state.login} />
-          <input onChange={this.handlePasswordChanged} placeholder='Password' type="password" value={this.state.password} />
-          <button onClick={this.handleSubmit}>Log-in</button>
-        </form>
-        { error && <p>Es ist ein Fehler aufgetreten!</p> }
-        <Link to="/signup">Noch keinen Account?</Link>
-      </div>
-    )
-  }
+
+    handlePasswordChanged = (event: Event) => {
+        if (event.target instanceof HTMLInputElement) {
+            this.setState({password: event.target.value})
+        }
+    }
+
+    handleSubmit = (event: Event) => {
+        event.preventDefault()
+        const {login, password} = this.state
+        this.props.authenticate(login, password, (error) => {
+            if (error) {
+                this.setState({error})
+            } else {
+                this.setState({redirectToReferrer: true, error: null})
+            }
+        })
+    }
+
+    render() {
+        const {from} = this.props.location.state || {from: {pathname: '/dashboard'}}
+        const {redirectToReferrer, error} = this.state
+
+        if (redirectToReferrer) {
+            return (
+                <Redirect to={from}/>
+            )
+        }
+
+        return (
+            <Card>
+                <Card.Content>
+                <Card.Header>Bank of Rapperswil</Card.Header>
+                <form className="ui form">
+                    <h2>Login</h2>
+                    <input className="ui input" onChange={this.handleLoginChanged} placeholder='Login'
+                           value={this.state.login}/>
+                    <input className="ui input" onChange={this.handlePasswordChanged} placeholder='Password'
+                           type="password" value={this.state.password}/>
+                    <button className="ui primary button" onClick={this.handleSubmit}>Log-in</button>
+                </form>
+                { error && <p>Es ist ein Fehler aufgetreten!</p> }
+                <Link to="/signup">Noch keinen Account?</Link>
+                </Card.Content>
+            </Card>
+        )
+    }
 }
 
 export default Login
